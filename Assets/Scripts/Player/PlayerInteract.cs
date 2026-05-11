e5275f7b7bb190ab1a7feef2e1eda17c19ded07e
Mitchel <mitchel_jager@hotmail.com>
2026-05-11 19:02:59 +0200

    Add interactable system
    Largely copied over from Space Bites Back

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Collections;
+using TMPro;
+
+public class PlayerInteract : MonoBehaviour {
+    public PlayerState playerState;
+    public Transform endTransform;
+    [Header("Cursor & UI")]
+    public GameObject interactImage;
+    public TMP_Text uiText;
+    [Header("Mask")]
+    public LayerMask layerMask;
+    [Header("Interact limits")]
+    public float interactRange = 2f;
+
+    private GameObject hitObject;
+    private Interactable hoveringInteractable;
+
+    void Update() {
+        hitObject = GetMouseHoverObject();
+
+        Debug.DrawLine(transform.position, endTransform.position, Color.red);
+
+        hoveringInteractable = UpdateGrabberTooltip();
+
+        interactImage.SetActive(hoveringInteractable);
+
+        if (hoveringInteractable && Input.GetMouseButtonDown(0)) hoveringInteractable.Interact();
+    }
+
+    private Interactable UpdateGrabberTooltip() {
+        if (hitObject == null || (!CanInteract())) {
+            uiText.text = " ";
+            uiText.gameObject.SetActive(false);
+            return null;
+        }
+
+        if (!hitObject.TryGetComponent<Interactable>(out var interactable)) return null;
+        if (!uiText.gameObject.activeInHierarchy) {
+            uiText.gameObject.SetActive(true);
+            uiText.text = interactable.text;
+        }
+
+        return interactable;
+    }
+
+    public bool CanInteract() {
+        return hitObject && hitObject.CompareTag("Interactable");
+    }
+
+    public GameObject GetMouseHoverObject() {
+        Vector3 position = transform.position;
+        Vector3 target = position + transform.forward * interactRange;
+
+        Physics.Linecast(position, target, out RaycastHit raycastHit, layerMask);
+
+        endTransform.position = raycastHit.point;
+
+		if (raycastHit.collider == null) {
+            endTransform.position = target;
+            return null;
+        }
+
+        return raycastHit.collider.gameObject;
+	}
+}
