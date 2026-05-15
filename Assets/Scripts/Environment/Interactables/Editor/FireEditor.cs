a5aa1e220e4923183e291f4b11b42c5aacadc6a2
Mitchel <mitchel_jager@hotmail.com>
2026-05-16 00:47:11 +0200

    Add additional fuel button to editor

--- Assets/Scripts/Environment/Interactables/Editor/FireEditor.cs
+++ Assets/Scripts/Environment/Interactables/Editor/FireEditor.cs
@@ -8,13 +8,20 @@ public class FireEditor : Editor {
 
         Fire fire = (Fire)target;
 
-        if (EditorApplication.isPlaying && GUILayout.Button("Add fuel")) {
+        if (EditorApplication.isPlaying && GUILayout.Button("Add max fuel")) {
             GameObject fuelObject = new("Editor created fuel");
             fuelObject.AddComponent<Fuel>();
             Fuel fuel = fuelObject.GetComponent<Fuel>();
             fuel.maxFuel = fire.maxFuel;
 
-            Debug.Log(fire.maxFuel);
+            fire.activeFuel.Add(fuel);
+        }
+
+        if (EditorApplication.isPlaying && GUILayout.Button("Add 5 fuel")) {
+            GameObject fuelObject = new("Editor created fuel");
+            fuelObject.AddComponent<Fuel>();
+            Fuel fuel = fuelObject.GetComponent<Fuel>();
+            fuel.maxFuel = 5f;
 
             fire.activeFuel.Add(fuel);
         }
