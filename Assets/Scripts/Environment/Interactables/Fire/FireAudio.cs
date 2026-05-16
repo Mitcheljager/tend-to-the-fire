335a0db52e3a733e64027d86c6c59c4586fb515f
Mitchel <mitchel_jager@hotmail.com>
2026-05-16 20:34:43 +0200

    Use class for audio items with curve for each item

--- Assets/Scripts/Environment/Interactables/Fire/FireAudio.cs
+++ Assets/Scripts/Environment/Interactables/Fire/FireAudio.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 
+[System.Serializable]
+public class FireAudioItem {
+    public AudioHelper audioHelper;
+    public AnimationCurve volumeCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
+}
+
 public class FireAudio : MonoBehaviour {
     public Fire fire;
-    [Header("Audio")]
-    public AudioHelper[] audioHelpersBase;
-    public AnimationCurve volumeBaseCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
+    public FireAudioItem[] audioItems;
 
     void Update() {
         SetAudioValues();
     }
 
     private void SetAudioValues() {
-        foreach (AudioHelper audioHelper in audioHelpersBase) {
-            audioHelper.audioSource.volume = volumeBaseCurve.Evaluate(fire.currentMultiplier);
+        foreach (FireAudioItem item in audioItems) {
+            item.audioHelper.audioSource.volume = item.volumeCurve.Evaluate(fire.currentMultiplier);
         }
     }
 }
