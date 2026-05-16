d1daa384407708c0b2afc85c0fadd0b87779bde9
Mitchel <mitchel_jager@hotmail.com>
2026-05-16 14:29:56 +0200

    Add smoke effects to FireEffects

--- Assets/Scripts/Environment/Interactables/Fire/FireEffects.cs
+++ Assets/Scripts/Environment/Interactables/Fire/FireEffects.cs
@@ -3,11 +3,15 @@ using UnityEngine.VFX;
 
 public class FireEffects : MonoBehaviour {
     public Fire fire;
-    [Header("Effects")]
+    [Header("Embers")]
     public VisualEffect embersEffect;
     public AnimationCurve embersEffectRateCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
     public AnimationCurve embersEffectMaxVelocityCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
     public AnimationCurve embersEffectMaxLifetimeCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
+    [Header("Smoke")]
+    public VisualEffect smokeEffect;
+    public AnimationCurve smokeEffectRateCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
+    [Header("Fire")]
     public Renderer[] effectRenderers;
     public AnimationCurve effectFuzzinessCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
     public AnimationCurve effectContrastCurve = new(new Keyframe(0f, 1f), new Keyframe(0.75f, 1f), new Keyframe(1f, 0f));
@@ -21,6 +25,8 @@ public class FireEffects : MonoBehaviour {
         embersEffect.SetFloat("Rate", embersEffectRateCurve.Evaluate(fire.currentMultiplier));
         embersEffect.SetFloat("Max Y Velocity", embersEffectMaxVelocityCurve.Evaluate(fire.currentMultiplier));
         embersEffect.SetFloat("Max Lifetime", embersEffectMaxLifetimeCurve.Evaluate(fire.currentMultiplier));
+
+        smokeEffect.SetFloat("Rate", smokeEffectRateCurve.Evaluate(fire.currentMultiplier));
 
         foreach (Renderer effectRenderer in effectRenderers) {
             effectRenderer.material.SetFloat("_Fuzziness", effectFuzzinessCurve.Evaluate(fire.currentMultiplier));
