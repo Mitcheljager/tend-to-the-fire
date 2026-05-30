5ce83d89b47e38cf628697a6dbc150cc62b657ce
Mitchel <mitchel_jager@hotmail.com>
2026-05-30 12:35:34 +0200

    Add script to randomize position of selected objects

---
+++ Assets/Scripts/Tools/RandomizeSelectedObjects.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor;
+
+public class RandomizeSelectedObjects : EditorWindow {
+    private float randomRange = 5f;
+    private bool x = true;
+    private bool y = false;
+    private bool z = true;
+
+    [MenuItem("Tools/Randomize object positions")]
+    public static void ShowWindow() {
+        GetWindow<RandomizeSelectedObjects>("Randomize positions");
+    }
+
+    private void OnGUI() {
+        randomRange = EditorGUILayout.FloatField("Range", randomRange);
+
+        x = EditorGUILayout.Toggle("X", x);
+        y = EditorGUILayout.Toggle("Y", y);
+        z = EditorGUILayout.Toggle("Z", z);
+
+        if (GUILayout.Button("Randomize positions", GUILayout.Height(30))) RandomizePositions();
+    }
+
+    private void RandomizePositions() {
+        GameObject[] selectedObjects = Selection.gameObjects;
+
+        Undo.RecordObjects(System.Array.ConvertAll(selectedObjects, selectedObject => (Object)selectedObject.transform), "Randomize positions");
+
+        foreach (GameObject selectedObject in selectedObjects) {
+            Vector3 original = selectedObject.transform.position;
+
+            float newX = original.x + (x ? Random.Range(-randomRange, randomRange) : 0f);
+            float newY = original.y + (y ? Random.Range(-randomRange, randomRange) : 0f);
+            float newZ = original.z + (z ? Random.Range(-randomRange, randomRange) : 0f);
+
+            selectedObject.transform.position = new Vector3(newX, newY, newZ);
+        }
+    }
+}
