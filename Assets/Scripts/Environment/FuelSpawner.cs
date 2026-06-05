948c73cc9726d158153f9bba6d6bc166ed717e59
Mitchel <mitchel_jager@hotmail.com>
2026-06-05 21:45:58 +0200

    Add second twig mesh

--- Assets/Scripts/Environment/FuelSpawner.cs
+++ Assets/Scripts/Environment/FuelSpawner.cs
@@ -14,7 +14,7 @@ public class FuelSpawner : MonoBehaviour {
     public void Spawn() {
         if (!fuelSpawnersManager) fuelSpawnersManager = FindAnyObjectByType<FuelSpawnersManager>();
 
-        GameObject prefab = fuelPrefabs[Random.Range(0, fuelPrefabs.Length - 1)];
+        GameObject prefab = fuelPrefabs[Random.Range(0, fuelPrefabs.Length)];
         Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0, 360f), 0f);
 
         GameObject instantiatedObject = Instantiate(prefab, transform.position, randomRotation);
