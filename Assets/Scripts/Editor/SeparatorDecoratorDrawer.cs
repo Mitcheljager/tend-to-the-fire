ee71cc0888cc0901f38ea41caa601295bf1fe888
Mitchel <mitchel_jager@hotmail.com>
2026-06-20 11:52:49 +0200

    Word

--- Assets/Scripts/Editor/SeparatorDecoratorDrawer.cs
+++ Assets/Scripts/Editor/SeparatorDecoratorDrawer.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(SeparatorAttribute))]
 public class SeparatorDrawer : DecoratorDrawer {
     public override float GetHeight() {
-        SeparatorAttribute attr = (SeparatorAttribute)attribute;
+        SeparatorAttribute separator = (SeparatorAttribute)attribute;
 
-        return attr.padding + attr.thickness;
+        return separator.padding + separator.thickness;
     }
 
     public override void OnGUI(Rect position) {
-        SeparatorAttribute attr = (SeparatorAttribute)attribute;
-        float y = position.y + attr.padding;
+        SeparatorAttribute separator = (SeparatorAttribute)attribute;
+        float y = position.y + separator.padding;
 
-        Rect line = new(position.x, y, position.width, attr.thickness);
+        Rect line = new(position.x, y, position.width, separator.thickness);
 
         EditorGUI.DrawRect(line, Color.black);
     }
