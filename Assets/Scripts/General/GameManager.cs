38e384ccd9369ac6be580fed2d491b1f49e72f13
Mitcheljager <mitchel_jager@hotmail.com>
2026-08-17 19:02:53 +0200

    Add level timer and dialog at end of level

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour {
     [Header("Components")]
     public EnemyManager enemyManager;
     public Fire fire;
+    [Header("Config")]
+    public float endTimeSeconds = 60000f;
+    public UnityEvent onGameEnd;
     [Header("State")]
-    public bool hasGameStarted = false;
+    [Fade] public bool hasGameStarted = false;
+    [Fade] public bool hasGameEnded = false;
+    [Fade] public float currentTimeSeconds = 0f;
 
     private int initialMaxNumberOfEnemies = 0;
     private float initialFuelConsumptionPerSecond = 0f;
@@ -19,10 +25,24 @@ public class GameManager : MonoBehaviour {
         fire.fuelConsumptionPerSecond = 0f;
     }
 
+    void Update() {
+        if (!hasGameStarted) return;
+
+        currentTimeSeconds += Time.deltaTime;
+
+        if (currentTimeSeconds >= endTimeSeconds) EndGame();
+    }
+
     public void StartGame() {
         hasGameStarted = true;
 
         enemyManager.maxNumberOfEnemies = initialMaxNumberOfEnemies;
         fire.fuelConsumptionPerSecond = initialFuelConsumptionPerSecond;
+    }
+
+    public void EndGame() {
+        hasGameEnded = true;
+
+        onGameEnd.Invoke();
     }
 }
